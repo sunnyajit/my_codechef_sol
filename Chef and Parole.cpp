c79c87d72b092c35432bf53ad50f0f4d214047d6
Sunny Kumar <136143741+sunnyajit@users.noreply.github.com>
2024-11-10 01:14:16 +0530

    Create Chef and Parole.cpp

---
+++ Chef and Parole.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+// code written by sunny
+int main() {
+    int daysSpent; 
+    cin >> daysSpent;  
+    if (daysSpent >= 7) {
+        cout << "Yes" << endl;  
+    } else {
+        cout << "No" << endl;   
+    }
+
+    return 0;
+}
