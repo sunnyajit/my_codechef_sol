36494085964d5afa9eda963af0c7490f5ed02d52
Sunny Kumar <136143741+sunnyajit@users.noreply.github.com>
2024-11-14 21:15:44 +0530

    Create Clearance Sale.cpp

---
+++ Clearance Sale.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+// code written by sunny
+int main() {
+    int X;
+    cin >> X;
+    int free_tshirts = X / 2;
+    int total_tshirts = X + free_tshirts;
+    cout << total_tshirts << endl;
+    
+    return 0;
+}
