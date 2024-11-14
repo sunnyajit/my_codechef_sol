434ef5eca3357e268055ed0b47f98d9506a565c3
Sunny Kumar <136143741+sunnyajit@users.noreply.github.com>
2024-11-14 20:59:37 +0530

    Create AI is Coming.cpp

---
+++ AI is Coming.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+// code written by sunny
+int main() {
+    int X;
+    cin >> X;
+
+    if (X <= 60) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
