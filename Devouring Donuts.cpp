595ff1dff4d757870e9fe8784a5e5936b8489d1a
Sunny Kumar <136143741+sunnyajit@users.noreply.github.com>
2024-11-15 11:10:07 +0530

    Create Devouring Donuts.cpp

---
+++ Devouring Donuts.cpp
@@ -0,0 +1,13 @@
+#include<iostream>>
+using namespace std;
+// code written by sunny
+int main(){
+    int X,Y;
+    cin >> X >> Y;
+    // CHECk  total calories..
+    int t_c= X*Y;  // user 
+    cout<<t_c<<endl;;
+    
+    return 0;
+    
+}
