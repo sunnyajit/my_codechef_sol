00644105cb122cc8edb43e23765ce52363c6731f
Sunny Kumar <136143741+sunnyajit@users.noreply.github.com>
2024-11-15 10:55:36 +0530

    Create Television Channels.cpp

---
+++ Television Channels.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+// code written by sunny..
+int main() {
+	// your code goes here
+	int X;
+	cin >> X;
+	// for cal to no of working channels
+	int wr_chl = (X + 1) / 2;
+	cout << wr_chl << endl;
+	return 0;
+}
