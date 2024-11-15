3d183c8f742e071171c2a01acd4094a773517be2
Sunny Kumar <136143741+sunnyajit@users.noreply.github.com>
2024-11-15 10:50:02 +0530

    Create How to accept multiple integers on se.cpp

---
+++ How to accept multiple integers on se.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b,c,d,e,f,g,h,i;
+    // given the output of function...
+    cin >> a >> b;
+    cin >> c >> d >> e;
+    cin >> f >> g >> h >> i;
+    cout<< a <<" "<< b <<" "<< c <<" "<< d <<" " << e <<" " << f << " "<<g << " " << h<<" "<< i << endl;
+}
