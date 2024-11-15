b9f42af61eabc20e8b3ac99f04498c57f341ec7f
Sunny Kumar <136143741+sunnyajit@users.noreply.github.com>
2024-11-15 11:13:56 +0530

    Create Counting Words.cpp

---
+++ Counting Words.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+// code written by sunny
+int main() {
+    int T;
+    cin >> T; 
+    while (T--) {
+        int N, M;
+        cin >> N >> M; 
+        int totalWords = N * M;
+        cout << totalWords << endl;
+    }
+
+    return 0;
+}
