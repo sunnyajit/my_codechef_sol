88aa410884da50e3cf6ebe5692d9ccc97de77ee2
Sunny Kumar <136143741+sunnyajit@users.noreply.github.com>
2024-11-15 11:13:02 +0530

    Create Giant Wheel.cpp

---
+++ Giant Wheel.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+// code written by sunny
+int main() {
+	int alc_ht;
+	cin>>alc_ht;
+	if(alc_ht>=60){
+	    cout<< "Yes" <<endl;
+	}else{
+	    cout<< "No" <<endl;
+	}
+
+}
