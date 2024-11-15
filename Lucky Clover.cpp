6c9479eb413877932d755b22bb8c6bf5c6898490
Sunny Kumar <136143741+sunnyajit@users.noreply.github.com>
2024-11-15 10:52:33 +0530

    Create Lucky Clover.cpp

---
+++ Lucky Clover.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+// code written by sunny..
+int main() {
+    int N;
+    cin >> N;
+    int totalLeaves = 3 * N + 1;
+    cout << totalLeaves << endl;
+    return 0;
+}
