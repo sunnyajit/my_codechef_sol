26a3360855d7d3ca677a2cff7272bac52f42e5f8
Sunny Kumar <136143741+sunnyajit@users.noreply.github.com>
2024-11-15 10:57:17 +0530

    Create Heat Wave.cpp

---
+++ Heat Wave.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+//code written by sunny
+int main() {
+	// your code goes here
+    // TAke the input case..
+    
+     int X,Y;
+     cin >> X  >> Y;
+     if(Y-X>0){
+         cout << "YES"  << endl;
+     } else{
+         cout << "NO" << endl;
+     }
+}
