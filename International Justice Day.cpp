3b75d96a540a75be52a35f4b9f6fae096a80af95
Sunny Kumar <136143741+sunnyajit@users.noreply.github.com>
2024-11-15 10:48:06 +0530

    Create International Justice Day.cpp

---
+++ International Justice Day.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+// code written by sunny...
+int main() {
+    int X, Y;
+    cin >> X >> Y;
+    if (X >= Y) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+    
+    return 0;
+}
