35d3e7f8f73086444a07a2c87682db395fafc146
Sunny Kumar <136143741+sunnyajit@users.noreply.github.com>
2024-11-15 11:29:17 +0530

    Create  Chef and Instant Noodles.cpp

---
+++ Chef and Instant Noodles.cpp
@@ -0,0 +1,10 @@
+ #include <iostream>
+// code written by sunny
+int main() {
+    int numStoves, numMinutes;
+    cin >> numStoves >> numMinutes;
+    int totalPackets = numStoves * numMinutes;
+     cout << totalPackets << endl;
+
+    return 0;
+}
