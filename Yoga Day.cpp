93120ef5a754b75708ac5be7d6876753433e4a1d
Sunny Kumar <136143741+sunnyajit@users.noreply.github.com>
2024-11-15 10:54:30 +0530

    Create Yoga Day.cpp

---
+++ Yoga Day.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+// code written by sunny...
+int main() {
+	// your code goes here
+	int N;
+	cin >> N;
+	// pass the test case.....
+	
+	
+	int posesePerRound = 12;
+	// cal the no of 
+	int completeRounds = N / posesePerRound;
+	// Output the number of complete rounds
+	cout << completeRounds << endl;
+	
+	return 0;
+
+}
