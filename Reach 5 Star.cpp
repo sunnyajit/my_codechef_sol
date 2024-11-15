4f5cd84430146a273f8c2c58ccabac2c6a6c6500
Sunny Kumar <136143741+sunnyajit@users.noreply.github.com>
2024-11-15 14:17:18 +0530

    Create Reach 5 Star.cpp

---
+++ Reach 5 Star.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+// code written by sunny
+int main() {
+	// your code goes here
+	int X,Y;
+	cin>>X>>Y;
+	
+	int newRating = X + Y;
+	
+	if(newRating >=2000)
+	   cout<<"YES"<<endl;
+	else
+	   cout<<"NO" << endl;
+	 
+	 return 0;
+	   
+	   
+	
+ 
+
+}
