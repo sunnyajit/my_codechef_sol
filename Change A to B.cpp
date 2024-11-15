53e5fae81db2bf8b5fd88b95173557fb860906cf
Sunny Kumar <136143741+sunnyajit@users.noreply.github.com>
2024-11-15 11:01:11 +0530

    Create Change A to B.cpp

---
+++ Change A to B.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long a, b, k;
+        cin >> a >> b >> k;
+        long long op = 0;
+        while (a != b)
+        {
+            if (b % k != 0)
+            {
+                long long f = b / k;
+                long long nearestMultiple = f * k;
+                op += (b - nearestMultiple);
+                b = nearestMultiple;
+            }
+            else if (b / k >= a)
+            {
+                b /= k;
+                op++;
+            }
+            else
+            {
+                op += (b - a);
+                break;
+            }
+        }
+        cout << op << endl;
+    }
+    return 0;
+}
