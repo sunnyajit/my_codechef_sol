10ad5b6cad5bf2fb20c18b782b6ac928d5d102d8
Sunny Kumar <136143741+sunnyajit@users.noreply.github.com>
2024-11-15 14:14:20 +0530

    Create Sub or Swp.cpp

---
+++ Sub or Swp.cpp
@@ -0,0 +1,14 @@
+ #include <bits/stdc++.h>
+using namespace std;
+ // code written by sunny
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int x, y;
+        cin>>x>>y;
+        
+        cout<<__gcd(x, y)<<"\n";
+    }
+}
