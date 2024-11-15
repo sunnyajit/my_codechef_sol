2976a0dfabfa596b208a2caa0226c0ec959915ea
Sunny Kumar <136143741+sunnyajit@users.noreply.github.com>
2024-11-15 11:02:42 +0530

    Create RCB vs CSK.cpp

---
+++ RCB vs CSK.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+// code written by sunny
+int main() {
+	int X,Y;
+	cin >> X >> Y;
+	if(X>=Y+18){
+	    cout<<"RCB"<<endl;
+	}else{
+	    cout<<"CSK"<<endl;
+	}
+}
